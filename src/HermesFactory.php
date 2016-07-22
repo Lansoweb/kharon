8ab90691ff9d83a22a221a4ced1523b23d8b722d
Leandro Silva <leandro.silva@zoox.com.br>
2016-07-22 14:51:52 -0300

    Updating to new factoryinterface but keeping previous compatibility

--- src/HermesFactory.php
+++ src/HermesFactory.php
@@ -1,26 +1,35 @@
 <?php
-
 namespace Metis;
 
-use Zend\ServiceManager\FactoryInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
 use Hermes\Api\ClientFactory;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 class HermesFactory implements FactoryInterface
 {
+    /**
+     * {@inheritDoc}
+     * @see \Zend\ServiceManager\Factory\FactoryInterface::__invoke()
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $hermesFactory = new ClientFactory();
+        $hermes = $hermesFactory->createService($container);
+
+        $metisFactory = new Factory();
+        $metis = $metisFactory->createService($container);
+
+        $hermes->setLoadBalance($metis);
+
+        return $hermes;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        $hermesFactory = new ClientFactory();
-        $hermes = $hermesFactory->createService($serviceLocator);
-
-        $metisFactory = new Factory();
-        $metis = $metisFactory->createService($serviceLocator);
-
-        $hermes->setLoadBalance($metis);
-
-        return $hermes;
+        return $this($serviceLocator, HermesFactory::class);
     }
 }
